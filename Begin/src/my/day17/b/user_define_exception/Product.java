e489eb3b2abdbeca1bfa10a12179c5ff0ac73e28
user <user@DESKTOP-RFK0USD>
2024-02-08 17:31:03 +0900

    24.02.08

--- Begin/src/my/day17/b/user_define_exception/Product.java
+++ Begin/src/my/day17/b/user_define_exception/Product.java
@@ -4,15 +4,9 @@ public interface Product {
 	
 	// 주문받기1
 	void order(int jumun_su) throws Jango_lack_Exception;		// 사용자가 정의하는 Jango_lack_ExceptionException
-	void jumun(int jumun_su) throws Jango_lack_Exception ;
-	
-	
-	
-	
 	
 	
 	// 주문받기2
-	
-	
+	void jumun(int jumun_su) throws Jango_lack_Exception ;
 
 }
