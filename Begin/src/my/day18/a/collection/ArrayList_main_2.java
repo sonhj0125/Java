49161178ab3e928806a87afb1d5f820bf8ac3068
user <user@DESKTOP-RFK0USD>
2024-02-08 15:31:58 +0900

    24.02.08

--- Begin/src/my/day18/a/collection/ArrayList_main_2.java
+++ Begin/src/my/day18/a/collection/ArrayList_main_2.java
@@ -30,6 +30,14 @@ public class ArrayList_main_2 {
         mbrList.add(new Member("kimth","Qwer1234$","김태희","0105064"));
         mbrList.add(new Member("kangkc","Qwer1234$","강감찬","9812301"));
         mbrList.add(new Member("kimss","Qwer1234$","김순신","0203203"));
+        
+        
+        Member mem1 = new Member("parksj","Qwer1234$","박서준","8812161");
+		Member mem2 = mem1;
+		mem2.setName("이현우");
+		
+		mbrList.add(mem1);	// 중복된 데이터를 저장할 수 있다.
+		mbrList.add(mem2);  // 중복된 데이터를 저장할 수 있다.
 		
         
         // mbrList 에 저장된 회원들의 정보를 출력하도록 한다.
@@ -57,6 +65,157 @@ public class ArrayList_main_2 {
            6. 만나이 : 55
            .................
         */
+        
+        
+        System.out.println("=====================================================\n");
+        
+        
+        for(Member mbr : mbrList) {
+        	System.out.println(mbr); 
+        } // end of for--------------------------------------
+        
+        
+        System.out.println("\n=====================================================\n");
+        
+        System.out.println("\n[퀴즈1] mbrList 에 저장되어진 모든 회원들중에 여자만 정보를 출력하세요 \n"); 
+        
+        for(Member mbr : mbrList) {
+            
+        	if("여".equals(mbr.gender())) {
+     			System.out.println(mbr);
+     			}
+     			
+     			
+        }// end of for--------------------------------------
+        
+        
+        System.out.println("\n[퀴즈2] mbrList 에 저장되어진 모든 회원들중에 성이 김씨인 회원만 정보를 출력하세요 \n");
+        
+        boolean is_existence = false;
+		/*
+		  for(Member mbr : mbrList) { 
+		  if(mbr.getName().startsWith("김")) { 
+		  is_existence = true; 
+		  System.out.println(mbr); 
+		  } 
+		  } // end of for--------------------------------------
+		*/
+        for(Member mbr : mbrList) {
+        	if(mbr.getName().indexOf("김") == 0) {
+        		 is_existence = true; 
+        		 System.out.println(mbr); 
+        	}
+        } // end of for--------------------------------------
+        
+        if(!is_existence) {
+        	System.out.println("김씨는 없습니다.");
+        }
+        
+        
+        
+        System.out.println("\n[퀴즈3] mbrList 에 저장되어진 모든 회원들중에 이름이 순신인 회원만 정보를 출력하세요 \n");
+              
+        is_existence = false;
+        
+        for(Member mbr : mbrList) {
+        	if(mbr.getName().endsWith("순신")) {
+        		 is_existence = true; 
+        		 System.out.println(mbr); 
+        	}
+        } // end of for--------------------------------------
+        
+        if(!is_existence) {
+        	System.out.println("이름이 순신인 회원은 없습니다.");
+        }
+        
+        
+        
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        
+        
+        // *** ArrayList 타입인 mbrList 에 새로운 Member 객체 추가시
+        //     특정 index(위치)에 들어가도록 할 수 있다. *** 
+        
+        System.out.println("\n ~~~ mbrList 에 새로운 Member 객체 추가하기 ~~~ \n"); 
+              
+        mbrList.add(new Member("seolh","Qwer1234$","설현","9910122"));
+        // index 값이 없으면 mbrList 의 맨 뒤에 추가된다.
+        System.out.println(mbrList.get(mbrList.size()-1));
+        
+        
+        System.out.println("\n######################################\n");
+        
+        mbrList.add(3, new Member("seokj","Qwer1234$","서강준","9901051"));
+        // 유재석(0) 엄정화(1) 강호동(2) 이순신(3) 으로 되어있었는데
+        // 유재석(0) 엄정화(1) 강호동(2) 서강준(3) 이순신(4) 으로 된다.     
+        
+        for(Member mbr : mbrList) {
+        		 System.out.println(mbr); 
+        } // end of for--------------------------------------
+        
+        
+        
+     // **** ArrayList 타입인 mbrList 에 저장되어진 Member 객체 삭제하기 **** //
+        System.out.println("\n **** ArrayList 타입인 mbrList 에 저장되어진 Member 객체 삭제하기 **** \n");
+        
+        System.out.println(">> 삭제하기 전 mbrList.size() => " + mbrList.size() + "\n");
+        Member deleted_mbr =  mbrList.remove(3);		// mbrList.remove(3); 은 삭제할 Member 객체의 index 번호
+        System.out.println(deleted_mbr);
+        
+       /*
+        	=== 서강준님의 회원정보 ===
+        	1. 아이디 : seokj
+        	2. 비밀번호: Qwer1234$
+        	3. 성명 : 서강준
+        	4. 주민번호 : 9901051
+        	5. 성별 : 남
+        	6. 만나이 : 25세
+		*/
+        
+        System.out.println(">> 삭제한 후 mbrList.size() => " + mbrList.size());
+        // >> 삭제한 후 mbrList.size() => 10
+        
+        
+        System.out.println("\n######################################\n");
+        
+        
+        for(Member mbr : mbrList) {
+        	System.out.println(mbr);
+        } // end of for--------------------------------------
+        
+        System.out.println("\n========================================\n");
+        
+        boolean is_delete = mbrList.remove(mbrList.get(0));		// index 0번 유재석을 넣어주면, 유재석이 지워짐. return 타입은 boolean
+        System.out.println(is_delete + "\n");
+        
+        for(Member mbr : mbrList) {
+        	System.out.println(mbr);
+        } // end of for--------------------------------------
+        
+        
+        
+        
+        System.out.println("\n[퀴즈3] mbrList 에 저장되어진 모든 회원들중에 이름이 순신인 회원만 정보를 출력하세요 \n");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
